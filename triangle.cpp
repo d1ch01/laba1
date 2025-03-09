4fc41fd0ef11738ec49692b247e9aa1df879e7d3
Roman <romanbogatyrev28@gmail.com>
2025-03-09 23:18:51 +0700

    Добавлены функции для треугольника

---
+++ triangle.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+// Функции для треугольника
+double trianglePerimeter(double a, double b, double c) {
+    return a + b + c;
+}
+
+double triangleArea(double a, double b, double c) {
+    double p = (a + b + c) / 2;
+    return sqrt(p * (p - a) * (p - b) * (p - c));
+}
+
+bool isIsosceles(double a, double b, double c) {
+    return (a == b) or (b == c) or (a == c);
+}
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a = 3, b = 4, c = 5;
+    cout << "\nТреугольник:" << endl;
+    cout << "Периметр: " << trianglePerimeter(a, b, c) << endl;
+    cout << "Площадь: " << triangleArea(a, b, c) << endl;
+    cout << "Равнобедренный: " << (isIsosceles(a, b, c) ? "Да" : "Нет") << endl;
+
+    return 0;
+}
