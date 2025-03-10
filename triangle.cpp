5839a35701d85431337183d6f3edfb5b6b90f291
d1ch01 <dimasikchelopcha@gmail.com>
2025-03-10 10:51:19 +0700

    Обновлен triangle.cpp в ветке triangle

--- triangle.cpp
+++ triangle.cpp
@@ -16,11 +16,24 @@ bool isIsosceles(double a, double b, double c) {
 }
 int main() {
     setlocale(LC_ALL, "RU");
-    double a = 3, b = 4, c = 5;
+    double a, b, c;
+    cout << "Введите значение первой стороны: ";
+    cin >> a;
+    cout << "Введите значение второй стороны: ";
+    cin >> b;
+    cout << "Введите значение третьей стороны: ";
+    cin >> c;
+    if (a < 0 || b < 0 ||c < 0) {
+        cout << "стороны не могут быть отрицательными." << endl;
+        return 1; // Завершение программы с кодом ошибки
+    }
+    if (a+b>c && a+c>b && c+b>a) {
     cout << "\nТреугольник:" << endl;
     cout << "Периметр: " << trianglePerimeter(a, b, c) << endl;
     cout << "Площадь: " << triangleArea(a, b, c) << endl;
     cout << "Равнобедренный: " << (isIsosceles(a, b, c) ? "Да" : "Нет") << endl;
-
+    }
+    else{
+      cout << "Не является треугольником";} 
     return 0;
 }
